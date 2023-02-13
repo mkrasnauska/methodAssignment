9aa13de8c3e04ead12735daa5bd50673298e3de3
Margarita <margaritakrasnauska@gmail.com>
2023-02-13 15:22:24 +0000

    Add project files.

---
+++ methodAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace methodAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please select a number to add");
+            int z = math.MyAdd(8, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Addition result is : " + z);
+            Console.WriteLine("Please select a number to substract");
+            int a = math.MySub(2, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Substraction result is : " + a);
+            Console.WriteLine("Please select a number to multiply");
+            int b = math.MyMult(3, Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Multiplication result is : " + b);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ methodAssignment/math.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace methodAssignment
+{
+    public class math
+    {
+        public static int MyAdd(int x, int y)
+        {
+            return x + y;
+        }
+        public static int MySub(int x, int y)
+        {
+            return x - y;
+        }
+        public static int MyMult(int x, int y)
+        {
+            return x * y;
+        }
+
+
+    }
+}
